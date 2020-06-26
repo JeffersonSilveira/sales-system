368becc20e3e02ea4b41650406fd7548cc6ada3c
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2020-06-26 13:16:29 -0300

    exemplo simples de configurations e beans

--- src/main/java/io/github/jeffersonsilveira/Configuration.java
+++
@@ -1,4 +0,0 @@
-package io.github.jeffersonsilveira;
-
-public class Configuration {
-}
---
+++ src/main/java/io/github/jeffersonsilveira/MinhaConfiguration.java
@@ -0,0 +1,13 @@
+package io.github.jeffersonsilveira;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinhaConfiguration {
+    @Bean(name ="applicationName")
+    public  String applicationName(){
+        return "Sistema de Vendas";
+    }
+
+}
