338e0dba9215e8a6e4d9faaad10ae38c61bce7dc
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2020-07-01 23:02:18 -0300

    server.port=8081

--- src/main/java/io/github/jeffersonsilveira/VendaApplication.java
+++ src/main/java/io/github/jeffersonsilveira/VendaApplication.java
@@ -2,6 +2,7 @@ package io.github.jeffersonsilveira;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -15,9 +16,7 @@ public class VendaApplication {
     public static void main(String[] args) {
         SpringApplication.run(VendaApplication.class, args);
     }
-
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${application.name}")
     private String applicationName;
 
     @GetMapping("hello")
