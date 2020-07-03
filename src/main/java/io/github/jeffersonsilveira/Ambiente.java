51578466bdd4e8a550a3a25f2ee9e165a80871bc
JeffersonSilveira <jeffersonsilveira30@gmail.com>
2020-07-02 21:31:47 -0300

    Criando annotation customizadas de configuração

---
+++ src/main/java/io/github/jeffersonsilveira/Ambiente.java
@@ -0,0 +1,5 @@
+package io.github.jeffersonsilveira;
+
+public interface Ambiente {
+    void selecionaAmbiente();
+}
--- src/main/java/io/github/jeffersonsilveira/VendaApplication.java
+++ src/main/java/io/github/jeffersonsilveira/VendaApplication.java
@@ -3,8 +3,10 @@ package io.github.jeffersonsilveira;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class VendaApplication {
+    @Development
+    private  Ambiente ambiente;
+
+    @Bean
+    public CommandLineRunner running(){
+        return  args -> {
+            this.ambiente.selecionaAmbiente();
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendaApplication.class, args);
